6ecf829003655df1b40eab351de701b9f9cb6d4d
SarojSatyarth <SarojSatyarth@SATYARTHPROGRAM>
2022-09-30 12:59:21 +0530

    Add project files.

---
+++ BasicAuth.API/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using BasicAuth.API.BasicAuth;
+using BasicAuth.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BasicAuth.API.Controllers
+{
+    [BasicAuthenticationAttribute]
+    public class EmployeesController : ApiController
+    {
+        public List<Employee> GetEmployees() 
+        {
+            return Employee.GetEmployees();
+        }
+    }
+}
